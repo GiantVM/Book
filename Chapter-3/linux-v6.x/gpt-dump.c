d915ca9349e2d805f4f557d6dacfbfa9753c168b
XU Kailiang <xukl2019@sjtu.edu.cn>
2024-12-03 21:08:11 +0800

    refactor(EPT lab): reduce code redundance

--- Chapter-3/linux-v6.x/gpt-dump.c
+++ Chapter-3/linux-v6.x/gpt-dump.c
@@ -92,140 +92,127 @@ MODULE_LICENSE("GPL");
 #define PADDR_PATTERN \
   PTE_PHYADDR_PATTREN VADDR_OFFSET_PATTERN
 
-static inline void pr_sep(void) {
+static inline void pr_sep(void)
+{
 	// pr_err(" ...........................................................................\n");
 	pr_err("\n");
 }
 
 /* static vals */
-unsigned long vaddr, paddr, pgd_idx, pud_idx, pmd_idx, pte_idx;
-const char *PREFIXES[] = {"PGD", "PUD", "PMD", "PTE"};
+const char *PAGE_LEVEL[] = { "PGD", "PUD", "PMD", "PTE" };
+enum { PGD, PUD, PMD, PTE };
 
 /* static inline functions */
 static inline void pr_pte(unsigned long address, unsigned long pte,
-                          unsigned long i, int level) {
-	if (level == 1)
+			  unsigned long i, int level)
+{
+	if (level == 0)
 		pr_cont(" NEXT_LVL_GPA(CR3)  =  ");
 	else
-		pr_cont(" NEXT_LVL_GPA(%s)  =  ", PREFIXES[level - 2]);
+		pr_cont(" NEXT_LVL_GPA(%s)  =  ", PAGE_LEVEL[level - 1]);
 	pr_cont(PTE_PHYADDR_PATTREN, UL_TO_PTE_PHYADDR(address >> PAGE_SHIFT));
 	pr_cont(" +  8 * %-3lu\n", i);
 	pr_sep();
 
-	pr_cont(" %-3lu: %s " PTE_PATTERN"\n", i, PREFIXES[level - 1], UL_TO_PTE(pte));
+	pr_cont(" %-3lu: %s " PTE_PATTERN "\n", i, PAGE_LEVEL[level],
+		UL_TO_PTE(pte));
 }
 
-static inline void print_ptr_vaddr(volatile unsigned long *ptr) {
+static inline void print_ptr_vaddr(volatile unsigned long *ptr,
+				   unsigned long idx[4])
+{
 	unsigned long mask = ((1 << 9) - 1);
 
-	vaddr = (unsigned long) ptr;
-	pgd_idx = (vaddr >> 39) & mask;
-	pud_idx = (vaddr >> 30) & mask;
-	pmd_idx = (vaddr >> 21) & mask;
-	pte_idx = (vaddr >> 12) & mask;
-	pr_info(" GPT PGD index: %lu", pgd_idx);
-	pr_info(" GPT PUD index: %lu", pud_idx);
-	pr_info(" GPT PMD index: %lu", pmd_idx);
-	pr_info(" GPT PTE index: %lu", pte_idx);
+	unsigned long vaddr = (unsigned long)ptr;
+	for (int i = 0; i < 4; ++i) {
+		idx[i] = (vaddr >> (39 - 9 * i)) & mask;
+		pr_info(" GPT %s index: %lu", PAGE_LEVEL[i], idx[i]);
+	}
 
-	pr_info("          %lu       %lu       %lu       %lu", pgd_idx, pud_idx, pmd_idx, pte_idx);
+	pr_info("          %lu       %lu       %lu       %lu",
+		       idx[PGD], idx[PUD], idx[PMD], idx[PTE]);
 	pr_info(" GVA [PGD IDX] [PUD IDX] [PMD IDX] [PTE IDX] [  Offset  ]");
-	pr_info(" GVA "VADDR_PATTERN"\n", UL_TO_VADDR(vaddr));
+	pr_info(" GVA " VADDR_PATTERN "\n", UL_TO_VADDR(vaddr));
 }
 
-static inline void print_pa_check(unsigned long vaddr) {
-	paddr = __pa(vaddr);
-	pr_info(" GPA            =      " PADDR_PATTERN "\n", UL_TO_PADDR(paddr));
+static inline void print_pa_check(unsigned long vaddr, unsigned long *paddr)
+{
+	*paddr = __pa(vaddr);
+	pr_info(" GPA            =      " PADDR_PATTERN "\n",
+		UL_TO_PADDR(*paddr));
 }
 
 /* page table walker functions */
-void dump_pgd(pgd_t *pgtable, int level);
+static void dump_pgd(pgd_t *pgtable, const unsigned long idx[4]);
 
-void dump_pud(pud_t *pgtable, int level);
+static void dump_pud(pud_t *pgtable, const unsigned long idx[4]);
 
-void dump_pmd(pmd_t *pgtable, int level);
+static void dump_pmd(pmd_t *pgtable, const unsigned long idx[4]);
 
-void dump_pte(pte_t *pgtable, int level);
+static void dump_pte(pte_t *pgtable, const unsigned long idx[4]);
 
-static int gpt_dump_init(void) {
+static int gpt_dump_init(void)
+{
 	volatile unsigned long *ptr;
 
 	ptr = kmalloc(sizeof(int), GFP_KERNEL);
-	*ptr = 1772333;
-	printk("Value at GVA: %lu", ++*ptr);
+	*ptr = 1772334;
+	printk("Value at GVA: %lu", *ptr);
 
-	print_ptr_vaddr(ptr);
-	dump_pgd(current->mm->pgd, 1);
-	print_pa_check(vaddr);
+	unsigned long idx[4];
+	print_ptr_vaddr(ptr, idx);
+	pr_sep();
+
+	dump_pgd(current->mm->pgd, idx);
+	unsigned long paddr;
+	print_pa_check((unsigned long)ptr, &paddr);
 
 	kvm_hypercall1(22, paddr);
 
-	kfree((const void *) ptr);
+	kfree((const void *)ptr);
 
 	return 0;
 }
 
 static void gpt_dump_cleanup(void) {}
 
-void dump_pgd(pgd_t *pgtable, int level) {
-	pr_sep();
-
-	unsigned long idx = pgd_idx;
-	if (!(0 <= idx && idx < PTRS_PER_PTE))
-		return;
-
-	pgd_t pgd = pgtable[idx];
-	if (pgd_val(pgd)) {
-		if (pgd_leaf(pgd)) {
-			pr_info("Large pgd detected! return");
-		} else if (pgd_present(pgd)) {
-			pr_pte(__pa(pgtable), pgd_val(pgd), idx, level);
-			dump_pud((pud_t *) pgd_page_vaddr(pgd), level + 1);
-		}
-	}
+#define define_dump(level, this_lvl, next_lvl, vaddr_func)                   \
+static void dump_##this_lvl(this_lvl##_t *pgtable,                           \
+			    const unsigned long idx[4])                      \
+{                                                                            \
+	unsigned long i = idx[level];				             \
+	if (!(0 <= i && i < PTRS_PER_PTE)) {                                 \
+		return;                                                      \
+	}                                                                    \
+	this_lvl##_t entry = pgtable[i];                                     \
+	if (this_lvl##_val(entry)) {                                         \
+		if (this_lvl##_leaf(entry)) {                                \
+			pr_info("Large %s detected! return",                 \
+				PAGE_LEVEL[level]);                          \
+		} else if (this_lvl##_present(entry)) {                      \
+			pr_pte(__pa(pgtable), this_lvl##_val(entry),         \
+			       i, level);                                    \
+			dump_##next_lvl(                                     \
+				(next_lvl##_t *)vaddr_func(entry),           \
+				idx);                                        \
+		}                                                            \
+	}                                                                    \
 }
 
-void dump_pud(pud_t *pgtable, int level) {
-	unsigned long idx = pud_idx;
-	if (!(0 <= idx && idx < PTRS_PER_PUD))
+define_dump(PGD, pgd, pud, pgd_page_vaddr)
+define_dump(PUD, pud, pmd, pud_pgtable)
+define_dump(PMD, pmd, pte, pmd_page_vaddr)
+
+static void dump_pte(pte_t *pgtable, const unsigned long idx[4])
+{
+	unsigned long i = idx[PTE];
+	if (!(0 <= i && i < PTRS_PER_PTE))
 		return;
 
-	pud_t pud = pgtable[idx];
-	if (pud_val(pud)) {
-		if (pud_leaf(pud)) {
-			pr_info("Large pud detected! return");
-		} else if (pud_present(pud) && !pud_leaf(pud)) {
-			pr_pte(__pa(pgtable), pud_val(pud), idx, level);
-			dump_pmd(pud_pgtable(pud), level + 1);
-		}
-	}
-}
-
-void dump_pmd(pmd_t *pgtable, int level) {
-	unsigned long idx = pmd_idx;
-	if (!(0 <= idx && idx < PTRS_PER_PMD))
-		return;
-
-	pmd_t pmd = pgtable[idx];
-	if (pmd_val(pmd)) {
-		if (pmd_leaf(pmd)) {
-			pr_info("Large pmd detected! return");
-		} else if (pmd_present(pmd) && !pmd_leaf(pmd)) {
-			pr_pte(__pa(pgtable), pmd_val(pmd), idx, level);
-			dump_pte((pte_t *) pmd_page_vaddr(pmd), level + 1);
-		}
-	}
-}
-
-void dump_pte(pte_t *pgtable, int level) {
-	unsigned long idx = pte_idx;
-	if (!(0 <= idx && idx < PTRS_PER_PTE))
-		return;
-
-	pte_t pte = pgtable[idx];
+	pte_t pte = pgtable[i];
 	if (pte_val(pte)) {
 		if (pte_present(pte)) {
-			pr_pte(__pa(pgtable), pte_val(pte), idx, level);
+			pr_pte(__pa(pgtable), pte_val(pte), i, PTE);
 		}
 	}
 }
